631fa667735256675397d526a64cc8604634621a
sunilgowda95 <105781130+sunilgowda95@users.noreply.github.com>
2022-10-13 08:18:07 +0530

    calculating average, maximum and minimum
    calculating average, maximum and minimum for input list.

--- statistics.py
+++ statistics.py
@@ -1,3 +1,13 @@
-
+import math
 def calculateStats(numbers):
-  return None
+  cs = {}
+  cs["avg"] = math.nan
+  cs["max"] = math.nan
+  cs["min"] = math.nan
+
+  if len(numbers) > 0:
+    cs["avg"] = sum(numbers) / len(numbers)
+    cs["max"] = max(numbers)
+    cs["min"] = min(numbers)
+    
+  return cs
